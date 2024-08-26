eea5652236a632eb0fe51df116106d038edd2b79
Damien <damcops1@gmail.com>
2024-08-26 09:08:01 +0200

    ...

--- query/csv3.py
+++ query/csv3.py
@@ -3,32 +3,6 @@ import csv
 
 conn = sqlite3.connect('data/vivino.db')
 cursor = conn.cursor()
-
-# avg wine price per country
-
-cursor.execute("""
-    SELECT countries.name, round(avg(vintages.price_euros)), vintages.ratings_average
-    FROM vintages
-    JOIN wines ON vintages.wine_id = wines.id
-    JOIN regions ON wines.region_id = regions.id
-    JOIN countries ON regions.country_code = countries.code
-    GROUP BY countries.name
-""")
-
-rows = cursor.fetchall()
-
-with open('data/second.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['country', 'priceAVG', 'rantingAVG']) 
-    csvwriter.writerows(rows)  
-
-conn.close()
-
-import sqlite3
-import csv
-
-conn = sqlite3.connect('data/vivino.db')
-
 # avg wine price per country
 
 cursor.execute("""

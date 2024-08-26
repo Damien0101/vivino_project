22a1212e6bb3f76609b1206d0c51c695b608318c
Damien <damcops1@gmail.com>
2024-08-26 12:30:36 +0200

    ...

---
+++ query/best_country.py
@@ -0,0 +1,22 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('data/vivino.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+    SELECT wines.name, vintages.price_euros, countries.name, wines.ratings_average, countries.wines_count
+    FROM wines
+    JOIN vintages ON wines.id = vintages.wine_id
+    JOIN regions ON wines.region_id = regions.id
+    JOIN countries ON regions.country_code = countries.code
+""")
+
+rows = cursor.fetchall()
+
+with open('data/best_countries.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['wine', 'price', 'country', 'rating', 'wine count']) 
+    csvwriter.writerows(rows)  
+
+cursor = conn.cursor()
---
+++ query/best_wineries.py
@@ -0,0 +1,23 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('data/vivino.db')
+cursor = conn.cursor()
+
+cursor.execute("""     
+    SELECT wines.name, ratings_count, ratings_average
+    FROM wines
+    ORDER BY ratings_count DESC
+    LIMIT 3;
+""")
+
+# I want a csv with wineries with the most wines produced and best rating for the wines
+
+rows = cursor.fetchall()
+
+with open('data/best_wineries.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['wine name', 'rating count', 'wine rating']) 
+    csvwriter.writerows(rows)  
+
+
